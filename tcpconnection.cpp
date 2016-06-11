b043b6d439c52ea4e07e6ab44a20f18bb4a7f88e
Yash <lodhad@gmail.com>
2016-06-10 23:12:21 -0700

    Added more mature read/write functions

--- tcpconnection.cpp
+++ tcpconnection.cpp
@@ -1,6 +1,9 @@
 #include "tcpserver.h"
+#include "cyexception.h"
 #include <netinet/in.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 cy::TCPConnection::TCPConnection() :
   _fd(-1)
@@ -41,12 +44,139 @@ cy::TCPConnection::~TCPConnection()
   this->server->_connectedClientsMutex.unlock();
 }
 
-int cy::TCPConnection::write(void* data, size_t len)
+
+
+//Write data to the socket connection
+ssize_t cy::TCPConnection::write(void* dptr, size_t len)
 {
-  return send(this->_fd, data, len, MSG_NOSIGNAL);
+  while(true)
+  {
+    ssize_t ret = send(this->_fd, dptr, len, MSG_NOSIGNAL);
+    if(ret < (ssize_t) len || ret < 0)
+    {
+      //We should continue to try to resend
+      if(errno == EINTR  || errno == ENOBUFS || errno == ENOMEM)
+      {
+        continue;
+      }
+
+      if(errno == EACCES || errno == EBADF || errno == ECONNRESET || errno == EDESTADDRREQ || errno == EFAULT || errno == EINVAL || errno == EMSGSIZE || errno == ENOTCONN || errno == ENOTSOCK || errno == EOPNOTSUPP || errno == EPIPE)
+      {
+        throw CYEXCEPTION("There is an issue with the socket or the connection. Thus, it is important that we end this connection!", errno);
+      }
+    }
+
+    return ret;
+  }
 }
 
-int cy::TCPConnection::read(void* arr, size_t len)
+//Write data to the socket connection
+ssize_t cy::TCPConnection::write(const char* str)
 {
-  return recv(this->_fd, arr, len, MSG_NOSIGNAL);
+  ssize_t len = strlen(str);
+  while(true)
+  {
+    ssize_t ret = send(this->_fd, str, len, MSG_NOSIGNAL);
+    if(ret < len || ret < 0)
+    {
+      //We should continue to try to resend
+      if(errno == EINTR  || errno == ENOBUFS || errno == ENOMEM)
+      {
+        continue;
+      }
+
+      if(errno == EACCES || errno == EBADF || errno == ECONNRESET || errno == EDESTADDRREQ || errno == EFAULT || errno == EINVAL || errno == EMSGSIZE || errno == ENOTCONN || errno == ENOTSOCK || errno == EOPNOTSUPP || errno == EPIPE)
+      {
+        throw CYEXCEPTION("There is an issue with the socket or the connection. Thus, it is important that we end this connection!", errno);
+      }
+    }
+
+    return ret;
+  }
+}
+
+//Write data to the socket connection
+ssize_t cy::TCPConnection::write(char* str)
+{
+  size_t len = strlen(str);
+  while(true)
+  {
+    ssize_t ret = send(this->_fd, str, len, MSG_NOSIGNAL);
+    if(ret < (ssize_t) len || ret < 0)
+    {
+      //We should continue to try to resend
+      if(errno == EINTR  || errno == ENOBUFS || errno == ENOMEM)
+      {
+        continue;
+      }
+
+      if(errno == EACCES || errno == EBADF || errno == ECONNRESET || errno == EDESTADDRREQ || errno == EFAULT || errno == EINVAL || errno == EMSGSIZE || errno == ENOTCONN || errno == ENOTSOCK || errno == EOPNOTSUPP || errno == EPIPE)
+      {
+        throw CYEXCEPTION("There is an issue with the socket or the connection. Thus, it is important that we end this connection!", errno);
+      }
+    }
+
+    return ret;
+  }
+}
+
+//Write data to the socket connection
+ssize_t cy::TCPConnection::write(char ch)
+{
+  while(true)
+  {
+    ssize_t ret = send(this->_fd, &ch, sizeof(char), MSG_NOSIGNAL);
+    if(ret < (ssize_t) sizeof(char) || ret < 0)
+    {
+      //We should continue to try to resend
+      if(errno == EINTR  || errno == ENOBUFS || errno == ENOMEM)
+      {
+        continue;
+      }
+
+      if(errno == EACCES || errno == EBADF || errno == ECONNRESET || errno == EDESTADDRREQ || errno == EFAULT || errno == EINVAL || errno == EMSGSIZE || errno == ENOTCONN || errno == ENOTSOCK || errno == EOPNOTSUPP || errno == EPIPE)
+      {
+        throw CYEXCEPTION("There is an issue with the socket or the connection. Thus, it is important that we end this connection!", errno);
+      }
+    }
+
+    return ret;
+  }
+}
+
+ssize_t cy::TCPConnection::read_int(void* buffer, size_t len, int flags)
+{
+  while(true)
+  {
+    ssize_t ret = recv(this->_fd, buffer, len, flags);
+    if(ret <= (ssize_t) len || ret < 0)
+    {
+      if(errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR || errno == ENOMEM)
+        continue;
+      else if(errno == EBADF || errno == ECONNREFUSED || errno == EFAULT || errno == EINVAL || errno == ENOTCONN || errno == ENOTSOCK)
+        throw CYEXCEPTION("There is an issue with the socket or the connection. Thus, it is important that we end this connection!", errno);
+    }
+
+    return ret;
+  }
+}
+
+ssize_t cy::TCPConnection::read(void* buffer, size_t buflen)
+{
+  return this->read_int(buffer, buflen, 0);
+}
+
+ssize_t cy::TCPConnection::read(char& ch)
+{
+  return this->read_int(&ch, sizeof(char), 0);
+}
+
+ssize_t cy::TCPConnection::peek(void* buffer, size_t len)
+{
+  return this->read_int(buffer, len, MSG_PEEK);
+}
+
+ssize_t cy::TCPConnection::peek(char& ch)
+{
+  return this->read_int(&ch, sizeof(char), 0);
 }
