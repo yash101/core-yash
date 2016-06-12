85308b075ff8eb0a971c6061f796c14b28cc0ecb
Yash <lodhad@gmail.com>
2016-06-12 00:07:11 -0700

    Started working on TCPClient

--- tcpclient.cpp
+++ tcpclient.cpp
@@ -1,6 +1,44 @@
 #include "tcpclient.h"
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <stddef.h>
 
-TCPClient::TCPClient()
+#define INIT(port) _fd(-1), _port(port), _server(NULL), _server_address(NULL)
+cy::TCPClient::TCPClient() : INIT(0)
 {
+  _server = new struct hostent;
+  _server_address = new struct sockaddr_in;
 
+  memset(_server, 0, sizeof(struct hostent));
+  memset(_server_address, 0, sizeof(struct sockaddr_in));
+}
+
+cy::TCPClient::TCPClient(const char* address) : INIT(0)
+{
+  _server = new struct hostent;
+  _server_address = new struct sockaddr_in;
+
+  memset(_server, 0, sizeof(struct hostent));
+  memset(_server_address, 0, sizeof(struct sockaddr_in));
+}
+
+cy::TCPClient::TCPClient(const char* ipaddr, int port) : INIT(port)
+{
+  _server = new struct hostent;
+  _server_address = new struct sockaddr_in;
+
+  memset(_server, 0, sizeof(struct hostent));
+  memset(_server_address, 0, sizeof(struct sockaddr_in));
+}
+
+cy::TCPClient::TCPClient(long ipaddr, int port) : INIT(port)
+{
+  _server = new struct hostent;
+  _server_address = new struct sockaddr_in;
+
+  memset(_server, 0, sizeof(struct hostent));
+  memset(_server_address, 0, sizeof(struct sockaddr_in));
 }
--- tcpclient.h
+++ tcpclient.h
@@ -1,11 +1,49 @@
 #ifndef TCPCLIENT_H
 #define TCPCLIENT_H
+#include "core_yash.h"
+#include <sys/types.h>
 
-
-class TCPClient
+namespace cy
 {
-public:
-  TCPClient();
-};
+  class TCPClient
+  {
+  private:
+    int _fd;
+    int _port;
+
+    void* _server;
+    void* _server_address;
+
+  public:
+
+    TCPClient();
+    TCPClient(const char* address);
+    TCPClient(const char* ipaddr, int port);
+    TCPClient(long ipaddr, int port);
+
+    bool setConnectionIP(const char* address);
+    bool setConnectionIP(long ipaddr);
+    bool setConnectionPort(int port);
+
+    int connect();
+
+  private:
+
+    ssize_t write_int(void* dptr, size_t len, int flags);
+    ssize_t read_int(void* dptr, size_t len, int flags);
+
+  public:
+
+    ssize_t write(void* dptr, size_t len);
+    ssize_t write(const char* str);
+    ssize_t write(char* str);
+    ssize_t write(char ch);
+
+    ssize_t read(void* buffer, size_t buflen);
+    ssize_t read(char& ch);
+    ssize_t peek(void* buffer, size_t buflen);
+    ssize_t peek(char& ch);
+  };
+}
 
 #endif // TCPCLIENT_H
